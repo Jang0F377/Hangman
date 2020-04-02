397839c15abf6caf0513d3145d148f1dab97cfb5
Jang0F377 <mjgarrett7092@gmail.com>
2020-04-02 11:25:56 -0700

    first of 4/2

--- src/com/mattgarrett/hangman/Hangman.kt
+++ src/com/mattgarrett/hangman/Hangman.kt
@@ -1,12 +1,18 @@
 package com.mattgarrett.hangman
 import java.io.File
 
-
+// Const val of lives that never changes
 const val lives: Int = 5
-val selectedWord by lazy { selectWord() }
-var userGuess = readLine()
-var userGuess2 = userGuess.toString()[0]
-    get() = field.toLowerCase()
+// Three Vars to use!
+val selectedWord = selectWord() //inital var that will be the string to compare too will be plaintext but not printed
+var selectedWord2 = replaceLetters(selectedWord) //var that replaces the letters of first var with underscores
+var selectedWord3 = selectedWord2 // copy of above var to make changes to
+
+
+//val selectedWord by lazy { selectWord() }
+//var userGuess = readLine()
+//var userGuess2 = userGuess.toString()[0]
+//    get() = field.toLowerCase()
 
 
 // Function to select a word from file.
@@ -16,24 +22,27 @@ fun selectWord() = File("data/Words.txt")
     .first()
 
 
-
-
-fun guessLetters(letter: Char) {
-    var selection = selectedWord.toCharArray()
-    if (letter in ) {
-        print(letter)
+// Function to replace chosen word with underscores so player can see amount of letters in word
+private fun replaceLetters(phrase: String) =
+    phrase.replace(Regex(pattern = "[a-zA-Z]")) {
+        when (it.value) {
+            it.value -> " __ "
+            else ->  "<?>"
+        }
     }
+
+
+fun findMatches(arg: String?) {
+
+
 }
+
 
 
 fun main() {
-//    println(">> Please enter a letter: ")
-    guessLetters(userGuess2)
+
 
 }
-
-
-
 
 
 //// variable to hold the picked word in underscores
@@ -47,14 +56,6 @@ fun main() {
 //    println(guessThisWord)
 //    print("> Guess your letter: ")
 //}
-//// Function to replace chosen word with underscores so player can see amount of letters in word
-//private fun replaceLetters(phrase: String) =
-//    phrase.replace(Regex(pattern = "[a-zA-Z]")) {
-//        when (it.value) {
-//            it.value -> " __ "
-//            else ->  "<?>"
-//        }
-//    }
 
 //private fun reverseLetters(phrase: String) =
 //    phrase.replace(Regex(pattern = "[_]")) {
